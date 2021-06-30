3adbc09496965878ff3124e9e871663ad545e463
vye_apurata <vanessa@apurata.com>
2021-06-29 19:59:07 -0500

    First profile

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="head">
+      <div class="item">
+        <span><i class=""></i> vanessasantillana59@gmail.com</span>
+      </div>
+      <div class="item">
+        <span><i class=""></i> 918 527 596</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.head {
+  display: flex;
+  align-items: flex-start;
+  justify-content: right;
+}
+</style>
